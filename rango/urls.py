c620aa933a7a5b7b7a298758ae88dbc2eb5424b2
Jude Campbell <judecampbell@judes-air.home>
2020-02-05 18:01:31 +0000

    message

---
+++ rango/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from rango import views
+app_name = 'rango'
+urlpatterns = [
+path('', views.index, name='index'),
+#Ch3 exercise 1
+path(''rango/about/, views.about, name='about'),
+]
