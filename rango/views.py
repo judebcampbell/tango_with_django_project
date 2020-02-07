f7b20bb4e286b10a65c50c972c69c7a695ea1ca9
Jude Campbell <judecampbell@judes-air.home>
2020-02-07 16:22:55 +0000

    Chapter 4 exercises

--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.conf import settings
+
+
+
 
 def index(request):
-    return HttpResponse("<a href='rango/about/'>About </a> Rango says hey there partner!")
-
+    #boldmessage matches to boldmessage in template
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    #then return rendered response
+    return render(request, 'rango/index.html', context=context_dict)
+    
 def about(request):
-    return HttpResponse("Rango says here is the about page. <a href='/rango/'> INDEX </a>")
+
+    return render(request, 'rango/about.html')
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from rango import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -9,4 +11,4 @@ urlpatterns = [
    
     #above maps any URLs starting with rango/ to be handled by rango
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
