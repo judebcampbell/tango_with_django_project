5ca1050dc8111863c5604686b154215ad1751c63
Jude Campbell <judecampbell@judes-air.home>
2020-02-09 18:30:42 +0000

    Chapter 5 Exercises

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    
+    class Meta:
+        verbose_name_plural = 'Categories'
+    
+    def __str__(self):
+        return self.name
+
+class Page(models.Model):
+    Category = models.ForeignKey(Category, on_delete=models.CASCADE) #sets up one to many relationship
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+    
+    def __str__(self): #good practice to implement
+    #equivelent of toString method in java
+        return self.title
